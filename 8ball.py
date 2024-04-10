2f7206cf974fa1234372bb001d4df1989f2cb78d
jadenauville-pvcc <159823794+jadenauville-pvcc@users.noreply.github.com>
2024-04-10 14:23:26 -0400

    Add files via upload

---
+++ 8ball.py
@@ -0,0 +1,8 @@
+# Name: Jaden Auville
+#   Prog Purpose: This Magic 8-Ball code uses a Python tuple since the
+#   user does not have the ability to change the 8-Ball answers.
+#   However, the program could have used a Python list instead of a tuple.
+# NOTE: Tuples use parenthese; lists use square braces.
+
+import random
+answers_8_ball = 
